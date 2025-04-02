cf3d85429f41049ff79896f287cacd01bd95d183
GITikhsan <ikhsanuddin.ahmad.fauzi@gmail.com>
2025-04-03 05:55:12 +0700

    Refactor GuestRepository and add new controllers
    Updated GuestRepository.cs:
    - Changed namespace to SimpleHotelManagement.Model.Repository
    - Changed class GuestRepository from internal to public
    - Added using directives for MySql.Data.MySqlClient, ProgramHotel.Context, and ProgramHotel.Model.Entity
    - Added GetAll method to retrieve Guest objects from the database
    
    Updated SimpleHotelManagement.csproj:
    - Added compile includes for Controller\GuestController.cs, Controller\RoomController.cs, and Controller\SettingController.cs
    - Removed folder include for Controller\
    
    Added GuestController.cs:
    - Created GuestController class with a constructor initializing GuestRepository
    - Added GetAllGuest method to return all guests
    
    Added RoomController.cs:
    - Created internal RoomController class
    
    Added SettingController.cs:
    - Created internal SettingController class

---
+++ SimpleHotelManagement/Controller/GuestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using ProgramHotel.Model;
+using ProgramHotel.Model.Entity;
+using ProgramHotel.Model.Repository;
+
+namespace SimpleHotelManagement.Controller
+{
+    public class GuestController
+    {
+        public GuestController()
+        {
+            _repository = new GuestRepository();
+        }
+
+        public List<Guest> GetAllGuest()
+        {
+            return _repository.GetAll();
+        }
+    }
+}
--- SimpleHotelManagement/Model/Repository/GuestRepository.cs
+++ SimpleHotelManagement/Model/Repository/GuestRepository.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using ProgramHotel.Context;
+using ProgramHotel.Model.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace SimpleHotelManagement.Model.Repository
 {
-    internal class GuestRepository
+    public class GuestRepository
     {
+        public List<Guest> GetAll()
+        {
+            List<Guest> tamuList = new List<Guest>();
+
+            using (DbContext context = new DbContext())
+            {
+                string query = "SELECT ";
+                using (MySqlCommand cmd = new MySqlCommand(query, context.Connection))
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        tamuList.Add(new Guest
+                        {
+                            /*tamu_id = Convert.ToInt32(reader["tamu_id"]),
+                            namaDepan = reader["namaDepan"].ToString(),
+                            namaBelakang = reader["namaBelakang"].ToString(),
+                            nomorTelepon = reader["nomorTelepon"].ToString(),
+                            alamat = reader["alamat"].ToString(),
+                            nomor_KTP = reader["nomor_KTP"].ToString()*/
+                        });
+                    }
+                }
+            }
+
+            return tamuList;
+        }
     }
 }
